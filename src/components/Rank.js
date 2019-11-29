667672724606bc8d6843a454f0295b0bb13e5d82
wht <wht1835@163.com>
2019-11-29 18:07:42 +0800

    排行页

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -1,8 +1,109 @@
 import React from 'react'
+import './rank.scss'
 
 function Rank () {
   return (
-    <div>Rank</div>
+    <div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+      <div className='rank-cards'>
+        <div className='card-img'> <img src={require('./../assets/images/r1.jpg')} alt='' /></div>
+        <div className='card-detail'>
+          <p>1.没有钥匙的锁</p>
+          <p>2.阿司匹林</p>
+          <p>3.你想去哪我都陪你</p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/Singer.js
+++ src/components/Singer.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 function Singer () {
+  
   return (
-    <div>Singer</div>
+    <div>
+      Singer
+    </div>
   )
 }
 
